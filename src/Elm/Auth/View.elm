191fe018207355d56f897ad9dfbfb0cfe066d574
ShujiOba <shuji365630@gmail.com>
2018-07-09 11:28:31 +0900

    Login画面の書き換え完了

--- src/Elm/Auth/View.elm
+++ src/Elm/Auth/View.elm
@@ -1,22 +1,36 @@
 module Auth.View exposing (..)
 
 import Auth.Model exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html exposing (Html, text, img, i, span)
+import Html.Attributes exposing (src, style, class)
+import Html.Events exposing (onClick)
+import Bulma.Columns as Bulma exposing (..)
+import Bulma.Elements as Bulma exposing (..)
+import Bulma.Form as Bulma exposing (..)
+import Bulma.Modifiers as Bulma exposing (..)
+import Bulma.Modifiers.Typography as Bulma
+
+
+loginViewColumnsModifiers : ColumnsModifiers
+loginViewColumnsModifiers =
+    { multiline = False
+    , gap = Gap3
+    , display = DesktopAndBeyond
+    , centered = True
+    }
 
 
 view : Html Msg
 view =
-    div [ class "columns is-centered" ]
-        [ div
-            [ class "column" ]
-            [ div
-                [ class "has-text-centered title is-1" ]
+    columns loginViewColumnsModifiers
+        []
+        [ column columnModifiers
+            []
+            [ title H1
+                [ Bulma.textCentered ]
                 [ text "Sign in" ]
-            , figure
-                [ class "image is-128x128"
-                , style
+            , image (OneByOne X128)
+                [ style
                     [ ( "margin", "16px auto 32px" )
                     , ( "border-radius", "50%" )
                     ]
@@ -29,55 +43,44 @@ view =
                     ]
                     []
                 ]
-            , div
-                [ class "field is-grouped is-grouped-centered" ]
-                [ div
-                    [ class "control" ]
-                    [ button
-                        [ class "button is-large is-info"
-                        , onClick <| Login Twitter
-                        , style btnStyle
-                        ]
-                        [ span [ class "icon" ]
-                            [ i [ class "fab fa-twitter" ] [] ]
-                        , span [] [ text "Twitter" ]
-                        ]
-                    ]
-                ]
-            , div
-                [ class "field is-grouped is-grouped-centered" ]
-                [ div
-                    [ class "control" ]
-                    [ button
-                        [ class "button is-large"
-                        , onClick <| Login Google
-                        , style btnStyle
-                        ]
-                        [ span [ class "icon has-text-link" ]
-                            [ i [ class "fab fa-google" ] [] ]
-                        , span [] [ text "Google" ]
-                        ]
-                    ]
-                ]
-            , div
-                [ class "field is-grouped is-grouped-centered" ]
-                [ div
-                    [ class "control" ]
-                    [ button
-                        [ class "button is-large is-black"
-                        , onClick <| Login GitHub
-                        , style btnStyle
-                        ]
-                        [ span [ class "icon" ]
-                            [ i [ class "fab fa-github" ] [] ]
-                        , span [] [ text "GitHub" ]
-                        ]
-                    ]
-                ]
+            , loginBtn Twitter
+            , loginBtn Google
+            , loginBtn GitHub
             ]
         ]
 
-btnStyle : List (String, String)
+
+loginBtn : LoginType -> Field Msg
+loginBtn loginType =
+    let
+        ( str, color ) =
+            case loginType of
+                Twitter ->
+                    ( "fab fa-twitter", Info )
+
+                Google ->
+                    ( "fab fa-google", Default )
+
+                GitHub ->
+                    ( "fab fa-github", Black )
+    in
+        fields Bulma.Centered
+            []
+            [ controlButton
+                { buttonModifiers
+                    | color = color
+                    , size = Large
+                    , iconLeft = Just ( Standard, [], i [ class str ] [] )
+                }
+                []
+                [ onClick <| Login loginType
+                , style btnStyle
+                ]
+                [ span [] [ text <| toString loginType ] ]
+            ]
+
+
+btnStyle : List ( String, String )
 btnStyle =
     [ ( "padding", "0 128px" )
     , ( "margin-bottom", "16px" )
